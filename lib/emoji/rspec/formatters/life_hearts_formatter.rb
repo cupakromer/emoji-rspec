4b889bb38dce944bae0c2fbc5efc6c03d8edc6b2
coreyhaines <coreyhaines@gmail.com>
2013-03-01 15:59:41 -0600

    Switch formatters to use Base and simple display methods

--- lib/emoji/rspec/formatters/life_hearts_formatter.rb
+++ lib/emoji/rspec/formatters/life_hearts_formatter.rb
@@ -1,26 +1,21 @@
-require 'rspec/core/formatters/base_text_formatter'
+require_relative 'base'
 
 module Emoji
   module RSpec
     module Formatters
-
-      class LifeHeartsFormatter < ::RSpec::Core::Formatters::BaseTextFormatter
-        def example_passed(example)
-          super(example)
-          output.print green "\u{1f49a} "
+      class LifeHeartsFormatter < Base
+        def passed_display
+          "\u{1f49a} "
         end
 
-        def example_failed(example)
-          super(example)
-          output.print red "\u{1f494} "
+        def failed_display
+          "\u{1f494} "
         end
 
-        def example_pending(example)
-          super(example)
-          output.print yellow "\u{1f49b} "
+        def pending_display
+          "\u{1f49b} "
         end
       end
-
     end
   end
 end
