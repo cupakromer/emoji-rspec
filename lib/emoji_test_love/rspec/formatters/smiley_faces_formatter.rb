c0e9ecbb47bc791332cc1a86784bdb5f41bfe4d2
coreyhaines <coreyhaines@gmail.com>
2013-03-15 16:04:22 -0500

    Create smiley faces formatter that really works!

---
+++ lib/emoji_test_love/rspec/formatters/smiley_faces_formatter.rb
@@ -0,0 +1,16 @@
+module EmojiTestLove
+  class Smiles
+    def passed_display
+      "\u{1f60a} "
+    end
+
+    def failed_display
+      "\u{1f621} "
+    end
+
+    def pending_display
+      "\u{1f62c} "
+    end
+  end
+end
+EmojiTestLove::RSpecFormatter(EmojiTestLove::Smiles, "SmileyFaces")
