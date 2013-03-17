bd4500df104f844ba8ec2466e75e86694f3f8a5b
Aaron Kromer <aaron.kromer@gmail.com>
2013-03-17 12:04:28 -0400

    Move to new expect syntax. Minor grammar change.

--- spec/generating_formatters_spec.rb
+++ spec/generating_formatters_spec.rb
@@ -16,31 +16,35 @@ EmojiTestLove::RSpecFormatter(Foo.new)
 EmojiTestLove::RSpecFormatter(Foo.new, "OtherName")
 
 describe "Generating a formatter" do
+
   let(:output) { stub }
   let(:formatter) { EmojiTestLove::FooFormatter.new(output) }
   let(:provider) { Foo.new }
+
   it "creates a subclass of RSpecFormatter" do
-    formatter.is_a?(::RSpec::Core::Formatters::BaseTextFormatter).should be_true
+    expect(formatter).to be_a(::RSpec::Core::Formatters::BaseTextFormatter)
   end
 
   it "supports passing an alternate name" do
-    EmojiTestLove::OtherNameFormatter.new(nil).is_a?(::RSpec::Core::Formatters::BaseTextFormatter).should be_true
+    expect(EmojiTestLove::OtherNameFormatter.new(nil))
+      .to be_a(::RSpec::Core::Formatters::BaseTextFormatter)
   end
 
   it "supports a namespaced class"
 
-  it "delegates example_passed to the passed_display to Foo" do
+  it "delegates example_passed to the passed_display of Foo" do
     output.should_receive(:print).with(provider.passed_display)
     formatter.example_passed(example)
   end
 
-  it "delegates example_failed to the failed_display to Foo" do
+  it "delegates example_failed to the failed_display of Foo" do
     output.should_receive(:print).with(provider.failed_display)
     formatter.example_failed(example)
   end
 
-  it "delegates example_pending to the pending_display to Foo" do
+  it "delegates example_pending to the pending_display of Foo" do
     output.should_receive(:print).with(provider.pending_display)
     formatter.example_pending(example)
   end
+
 end
