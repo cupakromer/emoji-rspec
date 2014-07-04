7d5f99406d537d2639d64f35b4d4fd7340d5e6f0
Klaus Hartl <klaus.hartl@gmail.com>
2014-07-04 20:13:37 +0200

    Use `is_expected` in example one-liners.

--- spec/support/shared_examples_for_formatters.rb
+++ spec/support/shared_examples_for_formatters.rb
@@ -3,20 +3,20 @@ shared_examples "an Emoji Test Love Formatter" do
   describe "class" do
     subject { described_class }
 
-    it { should respond_to :names }
+    it { is_expected.to respond_to(:names) }
   end
 
   describe "instance" do
     subject { described_class.new(StringIO.new) }
 
-    it { should respond_to :example_passed  }
-    it { should respond_to :example_failed  }
-    it { should respond_to :example_pending }
-    it { should respond_to :dump_summary    }
-    it { should respond_to :dump_failures   }
-    it { should respond_to :dump_pending    }
-    it { should respond_to :passed_display  }
-    it { should respond_to :failed_display  }
-    it { should respond_to :pending_display }
+    it { is_expected.to respond_to(:example_passed)  }
+    it { is_expected.to respond_to(:example_failed)  }
+    it { is_expected.to respond_to(:example_pending) }
+    it { is_expected.to respond_to(:dump_summary)    }
+    it { is_expected.to respond_to(:dump_failures)   }
+    it { is_expected.to respond_to(:dump_pending)    }
+    it { is_expected.to respond_to(:passed_display)  }
+    it { is_expected.to respond_to(:failed_display)  }
+    it { is_expected.to respond_to(:pending_display) }
   end
 end
